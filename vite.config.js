cf9bb83debc8c490b8c6cc7b5adcfbd8b77e96e5
skonda29 <skonda29@asu.edu>
2025-07-28 21:58:48 -0700

    fixed cors issue

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,28 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+        headers: {
+          'Origin': 'http://localhost:5173'
+        }
+      }
+    }
+  }
 })
